037322c1445c78d752155d8dbdae859534b24fbc
nigarrahimova <108179209+nigarrahimova@users.noreply.github.com>
2022-06-26 00:51:29 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const price1 = 20;
+const price2 = 10;
+const price3 = 5;
+
+const totalPrice = price1 + price2 + (price3 * 2);
+
+const averagePrice = totalPrice / 4;
+
+const finalPrice = totalPrice - 5;
+
+console.log(totalPrice, averagePrice, finalPrice);
